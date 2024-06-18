8dc8effdc70957d4be8778a135e669186dc1e775
SimchaTeich <sim.teich@gmail.com>
2024-06-18 20:12:53 +0300

    fix task 2

--- Assignment1/Task2/myAPI/index.js
+++ Assignment1/Task2/myAPI/index.js
@@ -38,6 +38,14 @@ async function get_bucket_objects_list()
 const express = require('express');
 const app = express();
 
+// Enable CORS for all requests
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.get('/videoList', async (req, res) => {
     let result = await get_bucket_objects_list();
     console.log("GET request for /videoList");
--- Assignment1/Task2/myWebServer/index.js
+++ Assignment1/Task2/myWebServer/index.js
@@ -1,77 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
-app.get('/', async (req, res) => {
-    try {
-        const response = await fetch('http://localhost:3000/videoList');
-        const videoList = await response.json();
-        
-        // Prepare HTML with the video list
-        let html = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>UTube</title>
-            <style>
-                .container {
-                    display: flex;
-                }
-                table {
-                    border-collapse: collapse;
-                    width: 25%;
-                }
-                th, td {
-                    border: 1px solid #ddd;
-                    padding: 8px;
-                    text-align: left;
-                }
-                th {
-                    background-color: #f2f2f2;
-                }
-                .video-container {
-                    flex: 1;
-                }
-                .video-player {
-                    flex: 1;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <div class="video-container">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Video List</th>
-                            </tr>
-                        </thead>
-                        <tbody>`;
-
-        videoList.forEach(video => {
-            html += `<tr><td>${video}</td></tr>`
-        });
-
-        html += `       </tbody>
-                    </table>
-                </div>
-                <div class="video-player" id="videoPlayer">`;
-        // Prepare HTML with the video player here in Task3
-        
-        html +=`                </div>
-            </div>
-        </body>
-        </html>`;
-
-        res.send(html);
-    } catch (error) {
-        console.error('Error fetching video list:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, "/index.html"));
 });
-
 
 
 app.listen(PORT, () => {
