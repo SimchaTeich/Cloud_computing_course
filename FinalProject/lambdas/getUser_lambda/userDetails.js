6e17585f10f0dcd5ea45fd864bf929eb43e245a9
SimchaTeich <sim.teich@gmail.com>
2024-08-13 19:06:23 +0300

    start with get user by ID lambda

--- FinalProject/lambdas/getUser_lambda/userDetails.js
+++ FinalProject/lambdas/getUser_lambda/userDetails.js
@@ -1,14 +1,35 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
 exports.handler = async (event) => {
     console.log(event);
 
-    if (event.queryStringParameters && event.queryStringParameters.userID)
+    if (!event.queryStringParameters == null || event.queryStringParameters.userID == null)
     {
-        const userID = event.queryStringParameters.userID;
-        
-        
-        return {statusCode:200, body: JSON.stringify({userID: event.queryStringParameters.userID})};
+        return {statusCode:200, body: JSON.stringify({msg: "userID parameter is missing"})};
     }
+
+    // exstract userID
+    const userID = event.queryStringParameters.userID;
+
+    // get user details by userID
+    const params = {
+        TableName: process.env.USERS_TABLE_NAME,
+        Key: {userID: userID}
+    };
+
+    const response = await docClient.send(new GetCommand(params));
+    const item = response.Item;
+    // if (item)
+    // {
+    //     return {statusCode:200, body: JSON.stringify(item)};
+    // }
     
-    return {statusCode:200, body: JSON.stringify({msg: "userID parameter is missing"})};
-  };
+    //return {statusCode:200, body: JSON.stringify({msg: "userID doesnt exist"})};
+    return {statusCode:200, body: JSON.stringify(item)};
+
+};
   
