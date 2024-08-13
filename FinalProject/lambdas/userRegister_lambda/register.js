f7b493a8d49cb0545662f8883960656ab4cfa216
SimchaTeich <sim.teich@gmail.com>
2024-08-13 03:40:08 +0300

    mini register work with check for user existence

--- FinalProject/lambdas/userRegister_lambda/register.js
+++ FinalProject/lambdas/userRegister_lambda/register.js
@@ -1,8 +1,11 @@
+// src getCommand: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/example_dynamodb_GetItem_section.html
+// src putCommand: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/example_dynamodb_PutItem_section.html
+
 const AWS = require('aws-sdk');
 //const docClient = new AWS.DynamoDB.DocumentClient();
 const querystring = require('node:querystring'); 
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
+const { DynamoDBDocumentClient, GetCommand, PutCommand } = require('@aws-sdk/lib-dynamodb');
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
@@ -22,24 +25,34 @@ exports.handler = async (event) => {
   const password = parsedData['password'];
 
   // check if user is exist
-  const command = new GetCommand({
+  const getcommand = new GetCommand({
     TableName: process.env.USERS_TABLE_NAME,
     Key: {
       email: email,
     },
   });
 
-  const response = await docClient.send(command);
+  const response = await docClient.send(getcommand);
   const item = response.Item;
   if (item)
   {
     return {statusCode:200, body: "User Already Exist"};
   }
-  else
-  {
-    return {statusCode:200, body: "User doesnt Exist"}; // add user to DB here
-  }
   
+  // else, insert the new user
+  const putcommand = new PutCommand({
+    TableName: process.env.USERS_TABLE_NAME,
+    Item: {
+      email: email,
+      username: username,
+      password: password
+    },
+  });
+
+  const response2 = await docClient.send(putcommand);
+  //console.log(response2);
+  return {statusCode:200, body: JSON.stringify(response2)};
+
   // try{
   //   const params = {
   //     TableName: process.env.HITS_TABLE_NAME,
