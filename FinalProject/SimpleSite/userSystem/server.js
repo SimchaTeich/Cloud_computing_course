5967beef86cd118e32a7f7509d20ad92c2763046
SimchaTeich <sim.teich@gmail.com>
2024-08-20 13:30:27 +0300

    append the site for checking the API

---
+++ FinalProject/SimpleSite/userSystem/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Route to Homepage
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/static/register.html');
+});
+
+app.get('/userDetails', (req, res) => {
+    res.sendFile(__dirname + '/static/userDetails.html');
+});
+
+app.get('/deleteUser', (req, res) => {
+    res.sendFile(__dirname + '/static/deleteUser.html');
+});
+
+
+app.get('/uploadProfile', (req, res) => {
+    res.sendFile(__dirname + '/static/uploadProfile.html');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`This app is listening on port ${PORT}`);
+});
