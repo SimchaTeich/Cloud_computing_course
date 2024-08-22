af15374817bd731017b26869e2fefb2f97271b59
SimchaTeich <sim.teich@gmail.com>
2024-08-22 20:31:21 +0300

    start publish from sqs lambda is OK

--- FinalProject/cdk/lambdas/publishFromSqs_lambda/publishFromSqs.js
+++ FinalProject/cdk/lambdas/publishFromSqs_lambda/publishFromSqs.js
@@ -4,27 +4,52 @@
 /************************************************************************************************************************************/
 
 // Imports
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, GetCommand } = require('@aws-sdk/lib-dynamodb');
 const { SNSClient, PublishCommand } = require("@aws-sdk/client-sns");
+
+
+// DynamoDB clients
+const client = new DynamoDBClient();
+const docClient = DynamoDBDocumentClient.from(client);
+
 
 // sns client
 const snsClient = new SNSClient();
 
+
 /*
 * function subscribe email to sns topic
 */
-async function publish(topicArn, event)
+async function publish(topicArn, subject, message)
 {
-    const response = await snsClient.send(
+    await snsClient.send(
         new PublishCommand({
-          Subject: event.Records[0].messageAttributes.Subject.stringValue,
-          Message: event.Records[0].body,
+          Subject: subject,
+          Message: message,
           TopicArn: topicArn
         }),
     );
-
-    return response;
 }
 
+
 exports.handler = async (event) => {
-  const response = await publish('arn:aws:sns:us-east-1:160844318631:test-topic', event);
+    console.log(event);
+
+    for (const record of event.Records) {
+        const publisherUserID = record.messageAttributes.publisherUserID.stringValue;
+        const subject         = record.messageAttributes.Subject.stringValue;
+        const message         = record.body;
+
+        //--------------------------------------------------
+        // get the topicArn by publisherUserID
+        const response = await docClient.send(new GetCommand({
+            TableName: process.env.TOPICS_TABLE_NAME,
+            Key: {userID: publisherUserID}
+        }));
+        const topicArn = response.Item.topicArn;
+        //--------------------------------------------------
+        
+        await publish(topicArn, subject, message);
+    }
 };
