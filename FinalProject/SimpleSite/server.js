ffd9e202da72ab7de7be95affb12cacfde651f31
SimchaTeich <sim.teich@gmail.com>
2024-08-23 13:40:20 +0300

    divide the site dir by content

--- FinalProject/SimpleSite/server.js
+++ FinalProject/SimpleSite/server.js
@@ -7,30 +7,35 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 // Route to Homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 
 app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/static/register.html');
+    res.sendFile(__dirname + '/userSystem/static/register.html');
 });
 
 app.get('/userDetails', (req, res) => {
-    res.sendFile(__dirname + '/static/userDetails.html');
+    res.sendFile(__dirname + '/userSystem/static/userDetails.html');
 });
 
 app.get('/deleteUser', (req, res) => {
-    res.sendFile(__dirname + '/static/deleteUser.html');
+    res.sendFile(__dirname + '/userSystem/static/deleteUser.html');
 });
 
 
 app.get('/uploadProfile', (req, res) => {
-    res.sendFile(__dirname + '/static/uploadProfile.html');
+    res.sendFile(__dirname + '/userSystem/static/uploadProfile.html');
 });
 
 
 app.get('/subscribe', (req, res) => {
-    res.sendFile(__dirname + '/static/subscribe.html');
+    res.sendFile(__dirname + '/subscriptions/static/subscribe.html');
+});
+
+
+app.get('/msg2MySubscribers', (req, res) => {
+    res.sendFile(__dirname + '/subscriptions/static/msg2MySubscribers.html');
 });
 
 
